f8f6ecc72798ef2de8cbabb33dea95a2a8c78d09
RyanBoyl <101837928+RyanBoyl@users.noreply.github.com>
2022-04-22 16:50:44 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
-"""This program is a Jeopardy-like quiz game.
+"""
+This program is a Jeopardy-like quiz game.
 Users are asked to identify a certain answer given a prompt.
-There are two topics in the quiz: Geography and Math."""
+There are two topics in the quiz: Geography and Math.
+There is also a final, mystery question.
+"""
 __author__ = "Ryan Boyle"
 
 import time
